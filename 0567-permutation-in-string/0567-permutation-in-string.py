b1ac0a5a5b8761a8262d39a5a3ee7e19add69127
khushii gupta <khushiii.guptaa06@gmail.com>
2026-07-10 23:29:02 +0530

    Time: 19 ms (60.91%), Space: 19.4 MB (17.82%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2):
+            return False
+
+        s1_count = {}
+        window_count = {}
+
+        # Frequency of s1
+        for ch in s1:
+            s1_count[ch] = s1_count.get(ch, 0) + 1
+
+        left = 0
+
+        for right in range(len(s2)):
+
+            # Add current character
+            window_count[s2[right]] = window_count.get(s2[right], 0) + 1
+
+            # Keep window size equal to len(s1)
+            if right - left + 1 > len(s1):
+                window_count[s2[left]] -= 1
+
+                if window_count[s2[left]] == 0:
+                    del window_count[s2[left]]
+
+                left += 1
+
+            # Compare frequency maps
+            if window_count == s1_count:
+                return True
+
+        return False
